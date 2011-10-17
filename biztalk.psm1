625f515458ebfe21037918f9698f423948a1c9ce
Scott Banwart <sbanwart@rogue-technology.com>
2011-10-16 23:17:13 -0300

    Update biztalk.psm1

--- biztalk.psm1
+++ biztalk.psm1
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 
 if ($env:BTSINSTALLPATH) {
   Add-Type -Path "$env:BTSINSTALLPATH\Developer Tools\Microsoft.BizTalk.ExplorerOM.dll"
